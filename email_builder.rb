57a365fc928aa2e450cecb7f401da1a6e2a5ac28
sarahemm <github@sen.cx>
2025-06-10 21:28:25 +0000

    Now sends emails when a session is removed from the schedule

--- email_builder.rb
+++ email_builder.rb
@@ -34,4 +34,33 @@ def queue_emails_when_scheduled(proposal_id)
   end
 end
 
+def queue_emails_when_unscheduled(proposal_id, unscheduled_by)
+  proposal = Proposal[proposal_id]
 
+  interests = Interest.where(proposal_id: proposal_id)
+  interested = interests.map(:name)
+
+  subject = "Your BoF '#{proposal[:title]}' has been removed from the schedule"
+  proposer_tmpl = ERB.new(File.read('email-templates/unscheduled-to_proposer.erb'))
+
+  mail = Mail.new(
+    to_address: proposal[:submitter_email],
+    subject: subject,
+    body: proposer_tmpl.result(binding)
+  )
+  mail.save
+
+  # queue up mail for each interested person, too
+  interest_tmpl = ERB.new(File.read('email-templates/unscheduled-to_interests.erb'))
+  interests.each do |interest|
+    next unless interest[:email] and interest[:email] != ''
+    subject = "BoF '#{proposal[:title]}' has been removed from the schedule"
+
+    mail = Mail.new(
+      to_address: interest[:email],
+      subject: subject,
+      body: interest_tmpl.result(binding)
+    )
+    mail.save
+  end
+end
