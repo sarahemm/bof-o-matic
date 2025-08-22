0013d65c527ef7035fbc939069d4704f1f905505
sarahemm <github@sen.cx>
2025-08-22 20:38:38 +0000

    Emails about scheduling now include "add to calendar" links
    This also included adding MIME multipart email support, which is
    now implemented. Current links are for Google and Office 365, we can
    add different ones later if there's demand for it. iCal/ics files
    aren't implemented yet, that's in TODO now.

--- email_builder.rb
+++ email_builder.rb
@@ -1,4 +1,5 @@
 require 'erb'
+require 'add_to_calendar'
 
 def queue_emails_when_scheduled(proposal_id)
   proposal = Proposal
@@ -10,17 +11,41 @@ def queue_emails_when_scheduled(proposal_id)
   interested = interests.map(:name)
 
   subject = "Your BoF '#{proposal[:title]}' has been scheduled!"
+  # TODO: maybe due for a refactor
   proposer_tmpl = ERB.new(File.read('email-templates/scheduled-to_proposer.erb'))
+  proposer_html_tmpl = ERB.new(File.read('email-templates/scheduled-to_proposer.html.erb'))
+  calendar_links = AddToCalendar::URLs.new(
+    # TODO: add end time too
+    start_datetime: proposal[:start_time],
+    title: proposal[:title],
+    location: proposal[:room_name],
+    # TODO: this should live in a config file, do as part of timezone work
+    timezone: 'America/Los_Angeles'
+  )
+  
+  # add the HTML part, reformat links, etc.
+  body = format_multipart(
+    plain: proposer_tmpl.result(binding),
+    html: proposer_html_tmpl.result(binding)
+  )
   
   mail = Mail.new(
     to_address: proposal[:submitter_email],
     subject: subject,
-    body: proposer_tmpl.result(binding)
+    body: body
   )
   mail.save
 
   # queue up mail for each interested person, too
   interest_tmpl = ERB.new(File.read('email-templates/scheduled-to_interests.erb'))
+  interest_html_tmpl = ERB.new(File.read('email-templates/scheduled-to_interests.html.erb'))
+  
+  # add the HTML part, reformat links, etc.
+  body = format_multipart(
+    plain: interest_tmpl.result(binding),
+    html: interest_html_tmpl.result(binding)
+  )
+
   interests.each do |interest|
     next unless interest[:email] and interest[:email] != ''
     subject = "BoF '#{proposal[:title]}' has been scheduled!"
@@ -28,7 +53,7 @@ def queue_emails_when_scheduled(proposal_id)
     mail = Mail.new(
       to_address: interest[:email],
       subject: subject,
-      body: interest_tmpl.result(binding)
+      body: body
     )
     mail.save
   end
@@ -116,3 +141,26 @@ def queue_reminder_emails(proposal_id, reminder_minutes)
     mail.save
   end
 end
+
+def format_multipart(plain:, html:)
+  mp_body = <<~EOF
+    --boundary-string
+    Content-Type: text/plain; charset="utf-8"
+    Content-Transfer-Encoding: 7BIT
+    Content-Disposition: inline
+    
+    #{plain}
+    
+    --boundary-string
+    Content-Type: text/html; charset="utf-8"
+    Content-Transfer-Encoding: 7BIT
+    Content-Disposition: inline
+    
+    #{html}
+    
+    --boundary-string--
+  EOF
+
+  return mp_body
+end
+
