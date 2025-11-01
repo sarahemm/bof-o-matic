59754c5dce2d7f40c6169bef6fd9c6490244e474
Grant Dobbe <grant@dobbe.us>
2025-11-01 14:22:11 -0700

    - added jquery - removed &s to put scripts in background

--- run-app.sh
+++ run-app.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo "starting daemons..."
-ruby bom-reminderd &
-ruby bom-scheduled &
-ruby bom-maild &
-ruby bom-smsd &
+ruby bom-reminderd 
+ruby bom-scheduled 
+ruby bom-maild 
+ruby bom-smsd 
 
 echo "starting bof-o-matic"
 exec bundle exec ruby bof-o-matic -o 0.0.0.0 -p 4590
