c8cbe1589402ba2e218fd7aae2239a32fba0dcab
Grant Dobbe <grant@dobbe.us>
2025-11-01 21:15:23 -0700

    - solved the issue with printing from docker (I think) - added some clarity to the run-app script about what is being started when

--- run-app.sh
+++ run-app.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
 echo "starting daemons..."
-ruby bom-reminderd 
-ruby bom-scheduled 
-ruby bom-maild 
-ruby bom-smsd 
+echo "- bom-labeld"
+ruby bom-labeld
+echo "- bom-reminderd"
+ruby bom-reminderd
+echo "- bom-scheduled"
+ruby bom-scheduled
+echo "- bom-maild"
+ruby bom-maild
+echo "- bom-smsd"
+ruby bom-smsd
 
 echo "starting bof-o-matic"
 exec bundle exec ruby bof-o-matic -o 0.0.0.0 -p 4590
